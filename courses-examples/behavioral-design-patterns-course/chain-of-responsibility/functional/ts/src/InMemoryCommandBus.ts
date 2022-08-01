4faf00aad9232c8442e6885539c4d8ab13b0fe74
Rafa Gómez <rgomezcasas@gmail.com>
2022-08-01 12:28:15 +0200

    feat(behavioural-course): [ts][chain][functional] add functional example

--- courses-examples/behavioral-design-patterns-course/chain-of-responsibility/functional/ts/src/InMemoryCommandBus.ts
+++ courses-examples/behavioral-design-patterns-course/chain-of-responsibility/functional/ts/src/InMemoryCommandBus.ts
@@ -1,15 +1,15 @@
 import { Command } from "./Command";
 import { CommandHandler } from "./CommandHandler";
-import { LoggerMiddleware } from "./LoggerMiddleware";
-import { TransactionalMiddleware } from "./TransactionalMiddleware";
+import { functionalLoggerMiddleware } from "./LoggerMiddleware";
+import { functionalTransactionalMiddleware } from "./TransactionalMiddleware";
 
 export class InMemoryCommandBus {
   constructor(private readonly commandHandler: CommandHandler) {}
 
   dispatch(command: Command): void {
-    const transactionalMiddleware = new TransactionalMiddleware(this.commandHandler);
-    const loggerMiddleware = new LoggerMiddleware(transactionalMiddleware);
+    const transactionalMiddleware = functionalTransactionalMiddleware(this.commandHandler.handle);
+    const loggerMiddleware = functionalLoggerMiddleware(transactionalMiddleware);
 
-    loggerMiddleware.handle(command);
+    loggerMiddleware(command);
   }
 }
