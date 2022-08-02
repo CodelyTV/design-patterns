df9dd08d2377baf548aa51f835288cf4a0c88667
Rafa Gómez <rgomezcasas@gmail.com>
2022-08-02 09:29:59 +0200

    feat(behavioural-course): [ts][strategy][functional] add functional example

--- courses-examples/behavioral-design-patterns-course/strategy/achivements-functional/ts/src/FullstackGuruAchivement.ts
+++
@@ -1,9 +0,0 @@
-import { AchivementStrategy } from "./AchivementStrategy";
-
-export class FullstackGuruAchivement implements AchivementStrategy {
-  dealAchievements(learnerId: string, earnedPoints: number): void {
-    if (earnedPoints > 200) {
-      throw Error(learnerId);
-    }
-  }
-}
